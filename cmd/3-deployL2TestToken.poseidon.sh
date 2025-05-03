d7f35041318d08a54dee5070708f687752ce0df3
0xsyung <93363780+0xsyung@users.noreply.github.com>
2025-05-03 14:46:18 +0000

    Refactoring

--- cmd/3-deployL2TestToken.poseidon.sh
+++ cmd/3-deployL2TestToken.poseidon.sh
@@ -1,3 +1,2 @@
 #/bin/sh
 cast send 0x4200000000000000000000000000000000000012 "createOptimismMintableERC20(address,string,string)" $TEST_TOKEN_SEPOLIA_ADDRESS "TestToken" "TT" --private-key $PRIVATE_KEY --rpc-url https://poseidon-testnet.rpc.caldera.xyz/http --json | jq -r '.logs[0].topics[2]' | cast parse-bytes32-address
-# 0x4A3DCE4e7eFC5872ff0Ecde6e336E315D54458Ec
